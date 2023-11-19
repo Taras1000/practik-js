b0472b3406b4f205de4e4074e3631b844d2c5d05
Taras1000 <semkivtaras80@gmail.com>
2023-11-19 10:01:09 +0000

    task-1

---
+++ js/lesson-1.js
@@ -0,0 +1,9 @@
+// ЗАДАЧА 1
+// НАПИШІТЬ РЕЧЕННЯ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА
+// "ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ГАЛИНА, МЕНІ 35 РОКІВ, Я ВИВЧАЮ JAVASCRIPT"
+
+const name = "Galyna";
+const age = 35;
+const hobby = "Javascript";
+const message = `ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${hobby}`;
+console.log(message);
