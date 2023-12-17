dbd32806fee07060c0ab414c616206459fc4da1e
Taras1000 <semkivtaras80@gmail.com>
2023-12-17 10:18:45 +0000

    task-3

--- js/lesson-3.js
+++ js/lesson-3.js
@@ -40,3 +40,118 @@
 // };
 
 // makeProduct("Холодильник", 10000, showProduct);
+
+
+// ЗАДАЧА 3
+
+const scientists = [
+    {
+      name: "Albert",
+      surname: "Einstein",
+      born: 1879,
+      dead: 1955,
+      id: 1,
+    },
+    {
+      name: "Isaac",
+      surname: "Newton",
+      born: 1643,
+      dead: 1727,
+      id: 2,
+    },
+    {
+      name: "Galileo",
+      surname: "Galilei",
+      born: 1564,
+      dead: 1642,
+      id: 3,
+    },
+{
+      name: "Marie",
+      surname: "Curie",
+      born: 1867,
+      dead: 1934,
+      id: 4,
+    },
+    {
+      name: "Johannes",
+      surname: "Kepler",
+      born: 1571,
+      dead: 1630,
+      id: 5,
+    },
+    {
+      name: "Nicolaus",
+      surname: "Copernicus",
+      born: 1473,
+      dead: 1543,
+      id: 6,
+    },
+{
+      name: "Max",
+      surname: "Planck",
+      born: 1858,
+      dead: 1947,
+      id: 7,
+    },
+    {
+      name: "Katherine",
+      surname: "Blodgett",
+      born: 1898,
+      dead: 1979,
+      id: 8,
+    },
+    {
+      name: "Ada",
+      surname: "Lovelace",
+      born: 1815,
+      dead: 1852,
+      id: 9,
+    },
+
+{
+      name: "Lise",
+      surname: "Meitner",
+      born: 1878,
+      dead: 1968,
+      id: 11,
+    },
+    {
+      name: "Sarah E.",
+      surname: "Goode",
+      born: 1855,
+      dead: 1905,
+      id: 10,
+    },
+    {
+      name: "Hanna",
+      surname: "Hammarström",
+      born: 1829,
+      dead: 1909,
+      id: 12,
+    },
+];
+  
+// МАЄМО МАСИВ, ПОТРІБНО:
+// 1)получити масив вчених які народились у 19ст;
+
+// const arrScientists = scientists.filter((scientist) =>
+//     scientist.born >= 1800 && scientist.born < 1900)
+//     .map((scientist) => `${scientist.name} ${scientist.surname}`);
+
+
+//     console.log(arrScientists);
+
+// 2)порахувати загальну суму років скільки прожили вчені,
+//     і визначити середнє значення;
+
+// const result = scientists.reduce((acc, { born, dead }) => acc += (dead - born) , 0);
+
+// console.log(result/scientists.length);
+
+// 3) відсортувати по алфавіту;
+
+// const sortedAlphabet = [...scientists].toSorted((a, b) =>
+//     (a.name > b.name) ? 1 : -1);
+
+//     console.log(sortedAlphabet);
